9fc98a6077bb45bc8628414f2f8fd5a4ab8e1110
abhishekp1703 <71189156+abhishekp1703@users.noreply.github.com>
2025-09-27 01:25:27 -0500

    Create LRUCache.java

---
+++ LRUCache.java
@@ -0,0 +1,16 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > capacity;
+    }
+}
